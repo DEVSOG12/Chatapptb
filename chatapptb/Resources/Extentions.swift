604d769f93a57dda6b7e8f0d99f3e651551ad9ca
George Solarin <oreofesolarin@gmail.com>
2020-08-31 16:20:18 +0100

    UI Changes

---
+++ chatapptb/Resources/Extentions.swift
@@ -0,0 +1,35 @@
+//
+//  Extentions.swift
+//  chatapptb
+//
+//  Created by Adaobi Solarin on 31/08/2020.
+//  Copyright © 2020 Adaobi Solarin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+extension UIView{
+    public var width: CGFloat {
+         return frame.size.width
+     }
+
+     public var height: CGFloat {
+         return frame.size.height
+     }
+
+     public var top: CGFloat {
+         return frame.origin.y
+     }
+
+     public var bottom: CGFloat {
+         return frame.size.height + frame.origin.y
+     }
+
+     public var left: CGFloat {
+         return frame.origin.x
+     }
+
+     public var right: CGFloat {
+         return frame.size.width + frame.origin.x
+     }
+}
