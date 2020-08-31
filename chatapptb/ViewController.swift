b303c1164311f80f1c37646f9245f61d2b6dd0e3
George Solarin <oreofesolarin@gmail.com>
2020-08-31 12:40:24 +0100

    Getting started

---
+++ chatapptb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatapptb
+//
+//  Created by Adaobi Solarin on 31/08/2020.
+//  Copyright © 2020 Adaobi Solarin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ chatapptbTests/chatapptbTests.swift
@@ -0,0 +1,34 @@
+//
+//  chatapptbTests.swift
+//  chatapptbTests
+//
+//  Created by Adaobi Solarin on 31/08/2020.
+//  Copyright © 2020 Adaobi Solarin. All rights reserved.
+//
+
+import XCTest
+@testable import chatapptb
+
+class chatapptbTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
